e5c793ef8cbd494a130ddb626bdc97a275323e70
Esa Koskinen <esakoskin2@gmail.com>
2018-06-26 19:37:12 +0900

    Initial commit.

---
+++ src/crystabit.cr
@@ -0,0 +1,6 @@
+require "./crystabit/*"
+
+# TODO: Write documentation for `Crystabit`
+module Crystabit
+  # TODO: Put your code here
+end
---
+++ src/crystabit/version.cr
@@ -0,0 +1,3 @@
+module Crystabit
+  VERSION = "0.1.0"
+end
