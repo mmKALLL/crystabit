7819cc5365f071890610f9d8b9f86f588d1b2173
Esa Koskinen <esakoskin2@gmail.com>
2018-06-27 20:48:30 +0900

    Fixed some specs. Changed ALU to use macro for Int type overloads.

--- spec/components/alu_spec.cr
+++ spec/components/alu_spec.cr
@@ -1,6 +1,6 @@
 require "../spec_helper"
 
-describe "ALU" do
+describe ALU do
   describe "operation handler" do
 
     describe "opcode 0 (return zero)" do
@@ -11,7 +11,7 @@ describe "ALU" do
       it "should sum two UInt64 values" do
         ALU.run_op(0, 1, 1).should eq 1
         ALU.run_op(123, 456, 1).should eq 579
-        ALU.run_op(0, 1, 1).should eq 1
+        ALU.run_op(2, 7, 1).should eq 9
       end
       it "should handle overflow (as in crystal??? or raise error? or using run_op overload?)" do
         # TODO
@@ -20,20 +20,46 @@ describe "ALU" do
 
     describe "opcode 2 (substract)" do
       it "should substract two UInt64 values" do
-        ALU.run_op(0, 1, 1)
+        ALU.run_op(1, 0, 2).should eq 1
+        ALU.run_op(20, 15, 2).should eq 5
+        ALU.run_op(2, 7, 2).should eq -5 # FIXME: undefined for UInt64
       end
     end
 
-    it "should raise (some exception???) on negative input" do
+    it "should raise (some exception???) on negative input of UInt params" do
       # TODO
     end
 
     it "should raise UnsupportedOpcodeException on unknown opcode" do
       expect_raises ALU.UnsupportedOpcodeException do
-        ALU.run_op(0, 1, 12347898)
+        ALU.run_op(2, 7, 12347898)
+      end
+    end
+
+    it "should raise UnsupportedOpcodeException on too large opcode" do
+      expect_raises ALU.UnsupportedOpcodeException do
+        ALU.run_op(2, 7, 128_i8)
       end
       expect_raises ALU.UnsupportedOpcodeException do
-        ALU.run_op(0, 1, -1)
+        ALU.run_op(2, 7, 256_u8)
+      end
+      expect_raises ALU.UnsupportedOpcodeException do
+        ALU.run_op(2, 7, 2147483648)
+      end
+    end
+
+    it "should raise UnsupportedOpcodeException on negative opcode" do
+      expect_raises ALU.UnsupportedOpcodeException do
+        ALU.run_op(2, 7, -1)
+      end
+    end
+
+    describe "type overload macro" do
+      it "should work on all unsigned integer sizes" do
+        # TODO
+      end
+      it "should work on all signed integer sizes" do
+
       end
     end
 
