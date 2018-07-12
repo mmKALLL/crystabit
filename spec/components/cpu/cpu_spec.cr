b1063db23f872d49ae37ca9e23f437e771b90b36
mmKALLL <esakoskin2@gmail.com>
2018-07-12 11:52:53 +0900

    Added CPU specs for running simple programs.

--- spec/components/cpu/cpu_spec.cr
+++ spec/components/cpu/cpu_spec.cr
@@ -1,5 +1,31 @@
 require "../spec_helper"
 
 describe CPU do
+  describe "execution handler" do
+    describe ", WASM mode" do
+      it "should execute all instructions" do
+        # See: https://github.com/sunfishcode/wasm-reference-manual/blob/master/WebAssembly.md#instructions
+        CPU.exec(0x01, Nil, []) # opcode, tuple of inputs, register state
+        # TODO
+      end
+      it "should run mnemonic assembly in text format" do
+        program = <<-WASM
+            nop
+            (local $res i32)
+            (set_local $res (i32.const 7))
+            (if
+              (i32.gt_s (
+                i32.add((2) (3))
+                (4)
+              ))
 
+              (set_local $res (
+                i32.add((2) (3))
+              ))
+            )
+            WASM
+        CPU.run(program)
+      end
+    end
+  end
 end
