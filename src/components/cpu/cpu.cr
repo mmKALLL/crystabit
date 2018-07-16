8f9b4283b2cecf4c1d7f38839c0a4423721300e2
Esa Koskinen <esakoskin2@gmail.com>
2018-07-16 18:36:15 +0900

    cpu.cr: Fixed syntax errors.

--- src/components/cpu/cpu.cr
+++ src/components/cpu/cpu.cr
@@ -2,16 +2,18 @@ module CPU
   extend self # All functions appended to CPU.self.
 
   # Executes a single operation and returns result as array of integers. Pure function. No memory access or state changes allowed.
-  def exec(opcode : Int, inputs : Array(Int), registers : Array(Int)) : Array(Int64)? {
+  def exec(opcode : Int, inputs : Array(Int), registers : Array(Int)) : Array(Int)?
     return Nil
-  }
+  end
 
   # Executes a single operation, with memory access and state changes returned
   def exec_full()
 
+  end
+
   # Run program, returning an array of memory accesses and state changes.
-  def run(program : String) : Array(Tuple(String, Int))? {
+  def run(program : String) : Array(Tuple(String, Int))?
     return [] of Tuple(String, Int32)
-  }
+  end
 
 end
